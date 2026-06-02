0379715f08faa2af73a6928baaa5cf23c2153845
Sinh Seavthay <sintion202201@gmail.com>
2026-06-02 13:41:56 +0700

    modify backend

---
+++ backend/employee_management/ems/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+from .views import *
+from rest_framework_simplejwt.views import TokenBlacklistView
+
+
+router = DefaultRouter()
+router.register(r'employee', views.EmployeeViewSet, basename='employee')
+router.register(r'department', views.DepartmentViewSet, basename='department')
+router.register(r'position', views.PositionViewSet, basename='position')
+router.register(r'payroll', views.PayrollViewSet, basename='payroll')
+router.register(r'leave', views.LeaveViewSet, basename='leave')
+router.register(r'attendance', views.AttendanceViewSet, basename='attendance')
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('', views.startproject),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/logout/', TokenBlacklistView.as_view(), name='logout'),
+]
---
+++ backend/employee_management/ems/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .serializer import *
+from .models import *
+from rest_framework import viewsets
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, UpdateModelMixin
+from .permissions import *
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def startproject(request):
+    return HttpResponse("Started Project")
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated, IsAdminUserRole]
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = [IsAuthenticated, IsAdminUserRole]
+
+class PositionViewSet(viewsets.ModelViewSet):
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
+    permission_classes = [IsAuthenticated, IsAdminUserRole]    
+
+class PayrollViewSet(CreateModelMixin, ListModelMixin, GenericViewSet):
+    queryset = Payroll.objects.all()
+    serializer_class = PayrollSerializer
+    permission_classes = [IsAuthenticated, IsManagerOrAdmin]  
+
+class LeaveViewSet(CreateModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = Leave.objects.all()
+    serializer_class = LeaveSerializer
+    permission_classes = [IsAuthenticated, IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if hasattr(user, 'employee') and user.employee.role in ['admin', 'manager']:
+            return Leave.objects.all()
+
+        return Leave.objects.filter(employee__user=user)
+
+class AttendanceViewSet(CreateModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if hasattr(user, 'employee') and user.employee.role in ['admin', 'manager']:
+            return Attendance.objects.all()
+        return Attendance.objects.filter(employee__user=user)
+    
+
+
+
+    
+        
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data = request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        username = serializer.validated_data.get('username')
+        password = serializer.validated_data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+
+            if user.is_active:
+
+                refresh = RefreshToken.for_user(user)
+
+                return Response({
+                    "message": "Login successful",
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Account is disabled"}, status=status.HTTP_403_FORBIDDEN)
+            
+        else:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
